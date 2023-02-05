b64c571bdfbbbfc20753f900338f3666831c5fd3
YaredBG <Yared_Gari@Mckinsey.com>
2023-02-04 16:07:18 -0800

    added course searching functionality

--- client/src/Components/SearchBar/SearchBar.js
+++ client/src/Components/SearchBar/SearchBar.js
@@ -1,18 +1,48 @@
+import { useContext, useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import styles from './SearchBar.module.css'
+import styles from "./SearchBar.module.css";
+
+import { MultiverseContext } from "../../ContextApi/contextapi";
 
 function SearchBar() {
+  const { courses, SetFilteredCourses } = useContext(MultiverseContext);
+  console.log(courses);
+
+  let inputRef = useRef(null);
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    try {
+      const userInput = inputRef.current.value?.toLowerCase();
+      if (!userInput || typeof userInput !== "string")
+        SetFilteredCourses(courses);
+      if (userInput || typeof userInput === "string") {
+        const foundCourses = courses.filter(
+          (course) =>
+            course?.coursetittle?.match(new RegExp(userInput, "i")) ||
+            course?.coursedescr?.match(new RegExp(userInput, "i"))
+        );
+        SetFilteredCourses(foundCourses);
+      }
+    } catch (error) {
+      console.log("something went wrong filter not working");
+    }
+  };
   return (
     <Form className="d-flex">
-    <Form.Control
-      type="search"
-      placeholder="Search your courses"
-      className="me-3"
-      aria-label="Search"
-    />
-    <Button className={styles.btn}>Search</Button>
-  </Form>
+      <Form.Control
+        type="search"
+        placeholder="Search your courses"
+        className="me-3"
+        aria-label="Search"
+        ref={inputRef}
+        onChange={handleSearch}
+      />
+      <Button className={styles.btn} onClick={handleSearch}>
+        Search
+      </Button>
+    </Form>
   );
 }
 
---
+++ client/src/Helper/Helper.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getAllCourses = () => {
+  let courses;
+  try {
+    courses = axios.get("url");
+  } catch (error) {
+    console.log('error occured when i try to fetch all courses');
+  }
+  return courses
+};
