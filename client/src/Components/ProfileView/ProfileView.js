40c6be52bc0eff4b7f60bc23512e24c877623fda
Jordan Hornback <jordanphornback@gmail.com>
2023-02-24 16:02:59 -0700

    Small update to profileview

--- client/src/Components/ProfileView/ProfileView.js
+++ client/src/Components/ProfileView/ProfileView.js
@@ -2,9 +2,26 @@ import React, {useContext} from 'react'
 import styles from'./ProfileView.module.css'
 import { MultiverseContext } from '../../ContextApi/contextapi'
 import Avatar from '../Avatar/Avatar'
+import cookieParser from "cookie-parser"
 
 
 function ProfileView() {
+
+  //finish once cookie is set up
+  // const user = await User.findByPk()
+  let user = { userID: 10,
+    firstName: "Jordan",
+    lastName: "Hornback",
+    password: "Password",
+    email: "email",
+    avatar: "/collegestudents.jpeg"
+  }
+
+  //let enrolledIn = user.getCourses()
+  //let completed = user.getCompleted()
+
+  let enrolledIn = 7
+  let completed = 5
 
   const {setIsEditClicked} = useContext(MultiverseContext)
 
@@ -17,13 +34,13 @@ function ProfileView() {
           <div className={styles.profile}>
 
             <div className={styles.profile_image}>
-              <img src={/*profileImg ||*/ "/defaultAvatar.jpeg"}/>
+              <img src={user.avatar|| "/defaultAvatar.jpeg"}/>
 
             </div>
 
             <div className={styles.profile_user_settings}>
 
-              <h1 className={styles.profile_user_name}>Student</h1>
+              <h1 className={styles.profile_user_name}>{user.firstName + " " + user.lastName}</h1>
 
               <button className={`${styles.btn} ${styles.profile_edit_btn}`} onClick={() => setIsEditClicked(true)}>Edit Profile</button>
 
@@ -34,8 +51,8 @@ function ProfileView() {
             <div className={styles.profile_stats}>
 
               <ul>
-                <li><span className={styles.profile_stat_count}>2</span> Enrolled in</li>
-                <li><span className={styles.profile_stat_count}>4</span> Completed</li>
+                <li><span className={styles.profile_stat_count}>Enrolled in </span>{enrolledIn ? enrolledIn : 2}</li>
+                <li><span className={styles.profile_stat_count}>Completed </span>{completed ? completed : 4}</li>
 
               </ul>
 
